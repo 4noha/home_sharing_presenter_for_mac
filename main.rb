3f3977ec53f6cf4e7ed1865664b81815f92f1e5c
野秋拓也 <nokkii.03@gmail.com>
2014-04-08 02:51:25 +0900

    ファーストコミット

---
+++ main.rb
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+require 'appscript'
+
+itunes = Appscript.app("iTunes")
+r = Regexp.new("PowerPoint|Impress|Chrome")
+
+while true
+  active_app = `osascript -e 'tell application "System Events" \n set front_app to name of (path to frontmost application) \n end tell'`
+  if nil != ( r =~ active_app )
+    begin
+      itunes.pause
+      code = ( "0b" + itunes.current_track.name.get ).oct if nil == code
+      case ( code - ( "0b" + itunes.current_track.name.get ).oct + 3 ) % 3
+      when 1
+        `osascript -e 'tell application "System Events" \n key code 123 \n end tell'`
+      when 2
+        `osascript -e 'tell application "System Events" \n key code 124 \n end tell'`
+      end
+      code = ( "0b" + itunes.current_track.name.get ).oct
+    rescue
+    end
+  end
+  sleep 0.1
+end
